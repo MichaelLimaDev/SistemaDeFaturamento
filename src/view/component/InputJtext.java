dda782167db8f2f082730266e6fe4fbb61819099
João Michael <117528496+MichaelLimaDev@users.noreply.github.com>
2022-11-21 12:37:39 +0000

    Criando input

---
+++ src/view/component/InputJtext.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class InputJtext extends JFrame {
+
+    private JTextField textField;
+
+    public void InputJtext() {
+        this.setTitle("Sei lá");
+        this.setSize(200, 120);
+
+        textField = new JTextField(10);
+    }
+}
